0338fbe944af2d83342a064839bdd8d71258a25b
Alex Henry <tukkek@gmail.com>
2024-03-26 00:00:00 +0000

    Quantity controls

--- script/view.js
+++ script/view.js
@@ -3,12 +3,21 @@ import * as rpg from './rpg.js'
 
 const VIEW=document.querySelector('#teams')
 const TEAM=document.querySelector('template#team').content.children[0]
+const NTEAMS=document.querySelector('input#nteams')
+const NFACTIONS=document.querySelector('input#nfactions')
 
-export function setup(){
-  for(let faction of rpg.shuffle(team.teams).slice(0,2)){
+function generate(){
+  for(let team of VIEW.querySelectorAll('.team')) team.remove()
+  for(let faction of rpg.shuffle(team.teams).slice(0,NTEAMS.value)){
     let t=TEAM.cloneNode(true)
     t.querySelector('.name').textContent=faction.name
-    t.querySelector('.factions').textContent=faction.generate(2).join(', ')
+    t.querySelector('.factions').textContent=faction.generate(NFACTIONS.value).join(', ')
     VIEW.appendChild(t)
   }
+}
+
+export function setup(){
+  NTEAMS.onchange=generate
+  NFACTIONS.onchange=generate
+  generate()
 }
